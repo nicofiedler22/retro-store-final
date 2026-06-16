19d7c500d6b486fa7aa302970eb29b07c1ae1196
unknown <Cetecom@SJAPCA03LC0707a.duoc.acad>
2026-06-16 10:53:17 -0400

    agregar application.properties y migraciones Flyway

---
+++ moduloInventario/src/main/resources/db/migration/V2__datos_iniciales.sql
@@ -0,0 +1,6 @@
+INSERT INTO inventario (producto_id, nombre, cantidad, stock_minimo, ubicacion, activo) VALUES
+    (1, 'Nintendo Entertainment System (NES)', 10, 3, 'Estante A1', TRUE),
+    (2, 'Super Nintendo (SNES)', 7, 3, 'Estante A2', TRUE),
+    (3, 'Super Mario Bros 3', 25, 5, 'Estante B1', TRUE),
+    (4, 'The Legend of Zelda: A Link to the Past', 15, 5, 'Estante B2', TRUE),
+    (5, 'Control NES original', 30, 10, 'Estante C1', TRUE);
