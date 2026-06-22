f685b3243240f31403dde9279a8feef090c7d08b
naxo18 <ign.floresm@duocuc.cl>
2026-06-21 22:48:04 -0400

    habilitar Eureka Discovery Client

--- inventario/src/main/java/cl/duoc/inventario/InventarioApplication.java
+++ inventario/src/main/java/cl/duoc/inventario/InventarioApplication.java
@@ -2,12 +2,12 @@ package cl.duoc.inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InventarioApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(InventarioApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(InventarioApplication.class, args);
+    }
 }
