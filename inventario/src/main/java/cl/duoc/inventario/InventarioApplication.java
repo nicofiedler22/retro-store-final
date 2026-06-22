247a5e0831400defae2e07542b6f4daf28dc9a94
naxo18 <ign.floresm@duocuc.cl>
2026-06-21 22:09:14 -0400

    solucion inventario

--- inventario/src/main/java/cl/duoc/inventario/InventarioApplication.java
+++ inventario/src/main/java/cl/duoc/inventario/InventarioApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventarioApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(InventarioApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioApplication.class, args);
+	}
+
 }
---
+++ inventario/src/test/java/cl/duoc/inventario/InventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
